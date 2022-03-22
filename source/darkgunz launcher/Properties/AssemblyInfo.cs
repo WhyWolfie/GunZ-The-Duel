f2759c264425a4940ade8fd99c0654c48a72aca5
WhyWolfie <59147206+WhyWolfie@users.noreply.github.com>
2022-03-22 12:45:15 +0100

    Add files via upload

---
+++ source/darkgunz launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: AssemblyCopyright("Copyright © iPlayOG - 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: CompilationRelaxations(8)]
+[assembly: AssemblyCompany("iPlayOG Network")]
+[assembly: AssemblyProduct("Coded by Wo1f (www.wo1f.net)")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyTitle("DarKGunZ Launcher")]
+[assembly: Guid("29ff6913-825f-4f37-bfc4-a858e5c9f362")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue)]
+[assembly: AssemblyVersion("1.0.0.0")]
