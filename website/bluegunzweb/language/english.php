07ca6b94dbabc970c010571f0c464722d1abc02d
WhyWolfie <59147206+WhyWolfie@users.noreply.github.com>
2022-08-18 11:35:30 +0200

    Add files via upload

---
+++ website/bluegunzweb/language/english.php
@@ -0,0 +1,344 @@
+<?php
+//Navigation
+$nav['1'] = "Home";
+$nav['2'] = "Download";
+$nav['3'] = "Register";
+$nav['4'] = "Forum";
+$nav['5'] = "Rankings";
+$nav['6'] = "Staff List";
+$nav['7'] = "Vote";
+$nav['8'] = "Item Shop";
+
+//Table Titles
+$title['1'] = "Login Panel";
+$title['2'] = "Server Info";
+$title['3'] = "Trailer";
+$title['4'] = "Links";
+$title['5'] = "Top 5 Player Rankings";
+$title['6'] = "Top 5 Clan Rankings";
+$title['7'] = "News &amp; Updates";
+$title['8'] = "Download";
+$title['9'] = "Register";
+$title['10'] = "Rankings";
+$title['11'] = "Staff List";
+$title['12'] = "Top Player Rankings";
+$title['13'] = "Top Clan Rankings";
+$title['14'] = "User Panel";
+$title['15'] = "Admin Panel";
+$title['16'] = "GameMaster Panel";
+$title['17'] = "Profile";
+$title['18'] = "Login";
+$title['19'] = "Logout";
+$title['20'] = "Item Shop";
+$title['21'] = "Newest Item";
+$title['22'] = "Forgotten Password";
+
+//Banned
+$banned['1'] = "Your account is banned";
+$banned['2'] = "Your IP is banned from";
+
+//Login Panel
+$logpanel['1'] = "UserID";
+$logpanel['2'] = "Password";
+$logpanel['3'] = "No Account?";
+$logpanel['4'] = "Forgotten Password?";
+$logpanel['5'] = "Register Here";
+$logpanel['6'] = "Click Here";
+$logpanel['7'] = "Welcome,";
+$logpanel['8'] = "Logout";
+
+//Server Info
+$servinfo['1'] = "Server Status :";
+$servinfo['2'] = "Players Online :";
+$servinfo['3'] = "EXP Rate :";
+$servinfo['4'] = "Newest Player :";
+$servinfo['5'] = "Total Accounts :";
+$servinfo['6'] = "Total Clans :";
+$servinfo['7'] = "Online";
+$servinfo['8'] = "Offline";
+$servinfo['9'] = "Total Characters :";
+
+//Links Images Title
+$linkimg['1'] = "Forum";
+$linkimg['2'] = "Vote";
+$linkimg['3'] = "Donate";
+
+//Player Rankings
+$plranks['1'] = "#";
+$plranks['2'] = "Name";
+$plranks['3'] = "Level";
+$plranks['4'] = "EXP";
+$plranks['5'] = "Player Rankings";
+
+//Clan Rankings
+$clranks['1'] = "#";
+$clranks['2'] = "Name";
+$clranks['3'] = "Points";
+$clranks['4'] = "Members";
+$clranks['5'] = "Clan Rankings";
+
+//Download Page
+$mirror['1'] = "Mirror1";
+$mirror['2'] = "Mirror2";
+$mirror['3'] = "Mirror3";
+
+//Group List
+$staff['1'] = "Administrator";
+$staff['2'] = "Head Staff";
+$staff['3'] = "Forum Moderator";
+$staff['4'] = "GameMaster";
+$staff['5'] = "Developer";
+$staff['6'] = "Member";
+$staff['7'] = "Banned";
+
+//Register Page
+$reg['1'] = "UserID";
+$reg['2'] = "Password";
+$reg['3'] = "Repeatpassword";
+$reg['4'] = "Email";
+$reg['5'] = "Name";
+$reg['6'] = "Age";
+$reg['7'] = "Register";
+$reg['8'] = "Please be sure that you enter your right information,
+             because if you forget your password then you can send your password to your email.";
+
+//Register Page Error Messages
+$regerror['1'] = "Username lenght is too long!";
+$regerror['2'] = "Account Created!";
+$regerror['3'] = "You have provided an invalid security code!";
+$regerror['4'] = "Passwords doesnt match!";
+$regerror['5'] = "This Email already exists!";
+$regerror['6'] = "Username already exists!";
+$regerror['7'] = "Please Fill All Fields!";
+$regerror['8'] = "You have been already registered!";
+
+//Login
+$login['1'] = "Processing...";
+$login['2'] = "Redirecting In";
+
+//Login Error Messages
+$logerror['1'] = "UserID doesnt exist!";
+$logerror['2'] = "Please enter userid and password!";
+
+//Logged In (Login Panel)
+$loinup['1'] = "Name";
+$loinup['2'] = "Age";
+$loinup['3'] = "Coins";
+$loinup['4'] = "Country";
+
+//Rankings Page (Player)
+$prankings['1'] = "#";
+$prankings['2'] = "Name";
+$prankings['3'] = "Level";
+$prankings['4'] = "EXP";
+$prankings['5'] = "Kills";
+$prankings['6'] = "Deaths";
+
+//Rankings Page (Clan)
+$crankings['1'] = "#";
+$crankings['2'] = "Name";
+$crankings['3'] = "Points";
+$crankings['4'] = "Wins";
+$crankings['5'] = "Losses";
+$crankings['6'] = "Draws";
+
+//Forgotten Password
+$fopass['1'] = "Send Password To Email"; 
+$fopass['2'] = "Your UserID"; 
+$fopass['3'] = "Your Email";
+$fopass['4'] = "Please enter UserID and Email!";
+$fopass['5'] = "This UserID doesnt exist!";
+$fopass['6'] = "Wrong Email!";
+$fopass['7'] = "Password sent to Email!";
+
+//UserPanel
+$userpanel['1'] = "Name";
+$userpanel['2'] = "Change Name";
+$userpanel['3'] = "Please enter your name!";
+$userpanel['4'] = "Age";
+$userpanel['5'] = "Change Age";
+$userpanel['6'] = "Please enter your age!";
+$userpanel['7'] = "Country";
+$userpanel['8'] = "Change Country";
+$userpanel['9'] = "Please enter your country!";
+$userpanel['10'] = "Please choose clan!";
+$userpanel['11'] = "Delete Clan";
+$userpanel['12'] = "Clan Deleted!";
+$userpanel['13'] = "Please choose sex!";
+$userpanel['14'] = "Sex changed!";
+$userpanel['15'] = "Change Sex";
+$userpanel['16'] = "Password";
+$userpanel['17'] = "Please enter password!";
+$userpanel['18'] = "Change Password";
+$userpanel['19'] = "Name changed!";
+$userpanel['20'] = "Age changed!";
+$userpanel['21'] = "Password changed!";
+$userpanel['22'] = "Country changed!";
+$userpanel['23'] = "Are you sure you want to delete your clan?";
+$userpanel['24'] = "Email";
+$userpanel['25'] = "Change Email";
+$userpanel['26'] = "Please enter email!";
+$userpanel['27'] = "Email changed!";
+
+//Admin & GM Panel
+$admingmp['1'] = "Change Character Name";
+$admingmp['2'] = "Old Name";
+$admingmp['3'] = "New Name";
+$admingmp['4'] = "Change";
+$admingmp['5'] = "Please enter old and new character name!";
+$admingmp['6'] = "Character name doesnt exist!";
+$admingmp['7'] = "Name changed!";
+$admingmp['9'] = "Change Character Level";
+$admingmp['10'] = "Name";
+$admingmp['11'] = "Level";
+$admingmp['12'] = "";
+$admingmp['13'] = "Please enter name and level!";
+$admingmp['14'] = "Level changed!";
+$admingmp['15'] = "Please enter name and exp!";
+$admingmp['16'] = "EXP changed!";
+$admingmp['17'] = "EXP";
+$admingmp['18'] = "Change Character EXP";
+$admingmp['19'] = "Change Character Sex";
+$admingmp['20'] = "Sex";
+$admingmp['21'] = "Sex changed!";
+$admingmp['22'] = "Please enter character name!";
+$admingmp['23'] = "Character Kill/DeathCount changed!";
+$admingmp['24'] = "Change Kill/DeathCount!";
+$admingmp['25'] = "Kills";
+$admingmp['26'] = "Deaths";
+$admingmp['27'] = "Character reseted!";
+$admingmp['28'] = "Reset";
+$admingmp['29'] = "Reset Character";
+$admingmp['30'] = "Change Password";
+$admingmp['31'] = "UserID";
+$admingmp['32'] = "Password";
+$admingmp['33'] = "Please enter userid and password!";
+$admingmp['34'] = "UserID doesnt exist!";
+$admingmp['35'] = "Password changed!";
+$admingmp['36'] = "Rank";
+$admingmp['37'] = "Change Rank";
+$admingmp['38'] = "Rank changed!";
+$admingmp['39'] = "Please enter userid!";
+$admingmp['40'] = "Create Clan";
+$admingmp['41'] = "Master";
+$admingmp['42'] = "Clan Name";
+$admingmp['43'] = "Create";
+$admingmp['44'] = "Please enter master and clan name!";
+$admingmp['45'] = "Master name doesnt exists!";
+$admingmp['46'] = "Clan name exists already!";
+$admingmp['47'] = "Clan created!";
+$admingmp['48'] = "Delete Clan";
+$admingmp['49'] = "Delete";
+$admingmp['50'] = "Please enter clan name!";
+$admingmp['51'] = "This clan name doesnt exist!";
+$admingmp['52'] = "Clan deleted!";
+$admingmp['53'] = "Change Clan Stats";
+$admingmp['54'] = "Wins";
+$admingmp['55'] = "Losses";
+$admingmp['56'] = "Draws";
+$admingmp['57'] = "Points";
+$admingmp['58'] = "Clan name doesnt exist!";
+$admingmp['59'] = "Clan Stats Changed!";
+$admingmp['60'] = "Please fill all fileds!";
+$admingmp['61'] = "Clan Score Reseted!";
+$admingmp['62'] = "Reset Clan";
+$admingmp['63'] = "Send Item";
+$admingmp['64'] = "ItemID";
+$admingmp['65'] = "Send";
+$admingmp['66'] = "Item Sent";
+$admingmp['67'] = "ItemID lenght too long! Max lenght is 9!";
+$admingmp['68'] = "Please enter character name and itemid!";
+$admingmp['69'] = "Delete";
+$admingmp['70'] = "Delete Item";
+$admingmp['71'] = "Item Deleted!";
+$admingmp['72'] = "Characters";
+$admingmp['73'] = "Accounts";
+$admingmp['74'] = "Clans";
+$admingmp['75'] = "Give Coins";
+$admingmp['76'] = "Add New Item";
+$admingmp['77'] = "Send";
+$admingmp['78'] = "Coins";
+$admingmp['79'] = "Please enter userid and coins!";
+$admingmp['80'] = "Coins Sent!";
+$admingmp['81'] = "Name";
+$admingmp['82'] = "ItemID";
+$admingmp['83'] = "Sex";
+$admingmp['84'] = "Level";
+$admingmp['85'] = "Price";
+$admingmp['86'] = "Category";
+$admingmp['87'] = "Type";
+$admingmp['88'] = "Image Location";
+$admingmp['89'] = "Damage";
+$admingmp['90'] = "Delay";
+$admingmp['91'] = "Magazine";
+$admingmp['92'] = "HP";
+$admingmp['93'] = "AP";
+$admingmp['94'] = "Weight";
+$admingmp['95'] = "Male";
+$admingmp['96'] = "Female";
+$admingmp['97'] = "All";
+$admingmp['98'] = "ranged";
+$admingmp['99'] = "malee";
+$admingmp['100'] = "armor";
+$admingmp['101'] = "sets";
+$admingmp['102'] = "Item added to Item Shop!";
+$admingmp['103'] = "Ranged";
+$admingmp['104'] = "Malee";
+$admingmp['105'] = "Armor";
+$admingmp['106'] = "Sets";
+$admingmp['107'] = "Ban IP";
+$admingmp['108'] = "IP";
+$admingmp['109'] = "Ban IP";
+$admingmp['110'] = "Please enter IP!";
+$admingmp['111'] = "IP Banned!";
+
+//User Profile
+$profile['1'] = "Name";
+$profile['2'] = "Age";
+$profile['3'] = "Country";
+$profile['4'] = "RegDate";
+$profile['5'] = "Level";
+$profile['6'] = "Kills";
+$profile['7'] = "Deaths";
+$profile['8'] = "#";
+$profile['9'] = "Characters";
+$profile['10'] = "No Characters";
+$profile['11'] = "Sex";
+
+//Item Shop
+$itmshop['1'] = "Ranged Weapons";
+$itmshop['2'] = "Malee Weapons";
+$itmshop['3'] = "Armors";
+$itmshop['4'] = "Set Items";
+$itmshop['5'] = "Type";
+$itmshop['6'] = "Sex";
+$itmshop['7'] = "Price";
+$itmshop['8'] = "Coins";
+$itmshop['9'] = "Buy";
+$itmshop['10'] = "Gift";
+$itmshop['11'] = "Please login first";
+$itmshop['12'] = "Level";
+$itmshop['13'] = "You dont have enough Coins";
+$itmshop['14'] = "Item sent to Central Bank";
+$itmshop['15'] = "Damage";
+$itmshop['16'] = "Delay";
+$itmshop['17'] = "Magazine";
+$itmshop['18'] = "HP";
+$itmshop['19'] = "AP";
+$itmshop['20'] = "Weight";
+$itmshop['21'] = "Item sent to Character Central Bank";
+$itmshop['22'] = "Character name doesnt exist";
+$itmshop['23'] = "Are you sure you want to buy this?";
+$itmshop['24'] = "Are you sure you want to gift this?";
+
+//Download
+$downl['1'] = "Download $gunz_name client here and come play with us. <br />
+	          If tou dont have an account yet, then make one now :";
+$downl['2'] = "Register Here";
+			  
+//Copyright
+$copyright['1'] = "Copyright &copy; 2011";
+$copyright['2'] = "All Rights Reserver";
+$copyright['3'] = "Website By";
+$copyright['4'] = "Rauno Leinasaar";  //Do Not Remove This
+?>
