678f637e7de231130d367e711c01bd536689a73d
WhyWolfie <59147206+WhyWolfie@users.noreply.github.com>
2022-09-08 01:28:01 +0200

    Add files via upload

---
+++ website/intro articgunz/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<!-- Mirrored from articgamers.net/ by Diablo Website Copier/3.x [XR&CO'2010], Fri, 01 Mar 2013 02:46:59 GMT -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>ArticGunz</title>
+<style>img{border:0;margin-top:20px;}a{text-decoration:none;border-style:none;}body{background-color:#222;}#contenedor{margin-top:20px;letter-spacing:55px;}#foot{margin-top:20px;width:771px;height:223px;background:url(imgs/linea_down.png) no-repeat;padding-top:90px;}</style>
+ 
+<script src="../ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(function() {
+	$('a.gunz').hover(function(){
+		$(this).find('img').attr("src","imgs/gunz_on.png");
+	}, function(){
+		$(this).find('img').attr("src","imgs/gunz_off.png");
+	});
+	
+	$('a.foro').hover(function(){
+		$(this).find('img').attr("src","imgs/forum_on.png");
+	}, function(){
+		$(this).find('img').attr("src","imgs/forum_off.png");
+	});
+	
+	$('a.staff').hover(function(){
+		$(this).find('img').attr("src","imgs/staff_on.png");
+	}, function(){
+		$(this).find('img').attr("src","imgs/staff_off.png");
+	});
+});
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32939737-1']);
+  _gaq.push(['_setDomainName', 'articgamers.net']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body>
+<center>
+<img src="imgs/linea_up.png"/>
+<div id="contenedor">
+<a class="gunz" href="http://201.211.160.142/Gunz/"><img src="imgs/gunz_off.png"/></a>
+<a class="foro" href="https://www.facebook.com/HeroStarGamers"><img src="imgs/forum_off.png"/></a>
+<a class="staff" href="https://www.facebook.com/skipchop"><img src="imgs/staff_off.png"/></a>
+</div>
+<div id="foot">
+<a href="https://www.facebook.com/skipchop"><img src="imgs/twitter.png"/></a> <a href="https://www.facebook.com/skipchop"><img src="imgs/face.png"/></a>
+<br>
+<br>
+<br>
+<br>
+<br>
+<script type="text/javascript">
+google_ad_client = "ca-pub-0317547545060935";
+/* web articgamers */
+google_ad_slot = "2954949127";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript" src="../pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+</div>
+</center>
+</body>
+
+<!-- Mirrored from articgamers.net/ by SkipChop Website Copier/3.x [XR&CO'2010], Fri, 01 Mar 2013 02:47:11 GMT -->
+</html>
